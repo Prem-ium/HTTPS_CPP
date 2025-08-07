052a03f3397851cba6849c22392187d0a3c41c5a
Prem <thefrostscoper@gmail.com>
2025-08-06 20:25:08 -0400

    Update https_client.cpp

--- https_client.cpp
+++ https_client.cpp
@@ -1,90 +1,63 @@
-#define _WIN32_WINNT 0x0601
+// mbedtls_https_client.cpp
 #include <iostream>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
+#include <cstring>
+#include <mbedtls/net_sockets.h>
+#include <mbedtls/ssl.h>
+#include <mbedtls/entropy.h>
+#include <mbedtls/ctr_drbg.h>
+#include <mbedtls/error.h>
+#include <mbedtls/debug.h>
 
-#ifdef _WIN32 // If compiling on Windows
-#include <winsock2.h>
-#include <ws2tcpip.h>  // inet_pton
-#pragma comment(lib, "ws2_32.lib") 
-#else // If compiling Unix-like systems
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#endif
-
-
-#define HOST "127.0.0.1"
-#define PORT 4433
+#define SERVER_PORT "4433"
+#define SERVER_ADDR "127.0.0.1"
 
 int main() {
-#ifdef _WIN32 //  declare Winsock; if windows
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
-#endif 
+    mbedtls_net_context server_fd;
+    mbedtls_ssl_context ssl;
+    mbedtls_ssl_config conf;
+    mbedtls_entropy_context entropy;
+    mbedtls_ctr_drbg_context ctr_drbg;
+    const char* pers = "ssl_client";
 
-    SSL_library_init();
-    SSL_load_error_strings();
-    OpenSSL_add_ssl_algorithms();
+    mbedtls_net_init(&server_fd);
+    mbedtls_ssl_init(&ssl);
+    mbedtls_ssl_config_init(&conf);
+    mbedtls_ctr_drbg_init(&ctr_drbg);
+    mbedtls_entropy_init(&entropy);
 
-    SSL_CTX* ctx = SSL_CTX_new(TLS_client_method());
-    if (!ctx) {
-        std::cerr << "Unable to create SSL context" << std::endl;
+    mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+                          (const unsigned char*)pers, strlen(pers));
+
+    mbedtls_net_connect(&server_fd, SERVER_ADDR, SERVER_PORT, MBEDTLS_NET_PROTO_TCP);
+
+    mbedtls_ssl_config_defaults(&conf,
+        MBEDTLS_SSL_IS_CLIENT,
+        MBEDTLS_SSL_TRANSPORT_STREAM,
+        MBEDTLS_SSL_PRESET_DEFAULT);
+    mbedtls_ssl_conf_authmode(&conf, MBEDTLS_SSL_VERIFY_NONE);
+    mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
+    mbedtls_ssl_setup(&ssl, &conf);
+    mbedtls_ssl_set_bio(&ssl, &server_fd, mbedtls_net_send, mbedtls_net_recv, nullptr);
+
+    if (mbedtls_ssl_handshake(&ssl) != 0) {
+        std::cerr << "Handshake failed\n";
         return 1;
     }
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("Socket creation failed");
-        return 1;
+    const char* req = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
+    mbedtls_ssl_write(&ssl, (const unsigned char*)req, strlen(req));
+
+    char buffer[1024];
+    int len = mbedtls_ssl_read(&ssl, (unsigned char*)buffer, sizeof(buffer) - 1);
+    if (len > 0) {
+        buffer[len] = '\0';
+        std::cout << "Server Response:\n" << buffer << std::endl;
     }
 
-    sockaddr_in addr{};
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-#ifdef _WIN32
-    if (inet_pton(AF_INET, HOST, &addr.sin_addr) <= 0) {
-        std::cerr << "Invalid address/ Address not supported\n";
-        return 1;
-    }
-#else
-    inet_aton(HOST, &addr.sin_addr);
-#endif
-
-
-    if (connect(sock, (sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("Connection failed");
-        return 1;
-    }
-
-    SSL* ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, sock);
-
-    if (SSL_connect(ssl) <= 0) {
-        ERR_print_errors_fp(stderr);
-    } else {
-        const char* req = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
-        SSL_write(ssl, req, strlen(req));
-
-        char buffer[4096];
-        int bytes = SSL_read(ssl, buffer, sizeof(buffer) - 1);
-        if (bytes > 0) {
-            buffer[bytes] = '\0';
-            std::cout << "Received:\n" << buffer << std::endl;
-        }
-    }
-
-    SSL_shutdown(ssl);
-    SSL_free(ssl);
-#ifdef _WIN32
-    closesocket(sock);
-    WSACleanup();
-#else
-    close(sock);
-#endif
-    SSL_CTX_free(ctx);
-    EVP_cleanup();
-
-    return 0;
+    mbedtls_ssl_close_notify(&ssl);
+    mbedtls_net_free(&server_fd);
+    mbedtls_ssl_free(&ssl);
+    mbedtls_ssl_config_free(&conf);
+    mbedtls_ctr_drbg_free(&ctr_drbg);
+    mbedtls_entropy_free(&entropy);
 }
